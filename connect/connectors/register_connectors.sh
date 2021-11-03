37f7fead60d28e9f562fccec5c51803a72c40c1d
Yangye Zhu <yangye.zhu@wishlife.com>
2021-11-02 22:11:35 -0700

    test: xtdb sink connector

--- connect/connectors/register_connectors.sh
+++ connect/connectors/register_connectors.sh
@@ -9,7 +9,7 @@ done
 echo -e $(date) "Kafka Connect is ready! Listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors)
 
 
-for connector in *.json; do
+for connector in xtdb*.json; do
   curl -s -X "POST" "http://localhost:8083/connectors" -H "Content-Type: application/json" -d @$connector
   sleep 10
 done
