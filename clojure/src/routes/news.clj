c3559f01d3e4a38e18783f65af9cb52bc2dbbc1b
Yangye Zhu <yangye.zhu@bryzi.io>
2021-11-03 17:05:07 -0700

    clojure: news routes

--- clojure/src/routes/news.clj
+++ clojure/src/routes/news.clj
@@ -5,13 +5,26 @@
             [taoensso.timbre :as log]
             [clj-http.client :as http]
             [cheshire.core :as json]
+            [common.util :as util]
+            [services.news :as sn]
             ))
 
 (defn all-news
   [{{:keys [id]} :path-params node :xtdb-node :as req}]
-  (-> (rr/ok {})
+  (-> (cond
+        (some? id)
+        (->> (sn/find-news-by-ids node [id])
+             )
+        
+        :default
+        (sn/find-all-news node {:limit 100}))
+      (util/camelize)
+      (rr/ok)
       (rr/content-type "application/json;charset=utf-8")))
 
 (def news-routes
   (r/routes (r/router [["/api/v1/news"
-                        ["" {:get all-news}]]])))
+                        ["" {:get all-news
+                             }]
+                        ["/:id" {:get all-news
+                                 }]]])))
