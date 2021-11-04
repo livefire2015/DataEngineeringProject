be8dea09f67618ffca8e75aa14e631ab7bd07dc9
Yangye Zhu <yangye.zhu@bryzi.io>
2021-11-03 17:04:55 -0700

    clojure: news service

--- clojure/src/services/news.clj
+++ clojure/src/services/news.clj
@@ -1,11 +1,21 @@
 (ns services.news
   (:require [taoensso.timbre :as log]
+            [common.util :as util]
+            [xtdb.api :as xt]
+            [common.xtdb.service :as xs]
             ))
+
+(def ^:const entity-type-news "rss_news")
+
 
 (defn find-news-by-ids
   [node ids]
-  )
+  (->> (xs/find-entities-by-ids node ids)
+       (filter #(= (:entity/type %) entity-type-news))
+       (map util/decrux)))
 
 (defn find-all-news
   [node {:keys [limit]}]
-  )
+  (->> {:limit limit}
+       (xs/find-entities-by-attrs node {:entity/type entity-type-news})
+       (map util/decrux)))
