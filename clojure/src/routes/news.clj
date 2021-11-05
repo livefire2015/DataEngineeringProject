1247bb5a767bbd3971be9697034bc0ac3e3ad206
Yangye Zhu <yangye.zhu@wishlife.com>
2021-11-05 12:01:00 -0700

    clojure: debug all-news tx

--- clojure/src/routes/news.clj
+++ clojure/src/routes/news.clj
@@ -5,12 +5,17 @@
             [taoensso.timbre :as log]
             [clj-http.client :as http]
             [cheshire.core :as json]
+            [xtdb.api :as xt]
             [common.util :as util]
+            [common.xtdb.service :as xs]
             [services.news :as sn]
             ))
 
 (defn all-news
   [{{:keys [id]} :path-params node :xtdb-node :as req}]
+  (log/debugf "Returns the latest transaction to have been indexed by this node: %s" (xt/latest-completed-tx node))
+  (log/debugf "Returns the latest transaction to have been submitted to this cluster: %s" (xt/latest-submitted-tx node))
+  (log/debugf "Returns frequencies of indexed attributes: %s" (xt/attribute-stats node))
   (-> (cond
         (some? id)
         (->> (sn/find-news-by-ids node [id])
