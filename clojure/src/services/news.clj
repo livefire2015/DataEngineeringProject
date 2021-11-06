fd9ef3374b7387ffc8bb3c75013b5ae3e4874813
Yangye Zhu <yangye.zhu@wishlife.com>
2021-11-05 17:10:12 -0700

    clojure: news service search wip

--- clojure/src/services/news.clj
+++ clojure/src/services/news.clj
@@ -2,8 +2,8 @@
   (:require [taoensso.timbre :as log]
             [common.util :as util]
             [xtdb.api :as xt]
-            [common.xtdb.service :as xs]
-            ))
+            [common.xtdb.service :as xs]))
+
 
 (def ^:const entity-type-news "rss_news")
 
@@ -19,3 +19,26 @@
   (->> {:limit limit}
        (xs/find-entities-by-attrs node {:entity/type entity-type-news})
        (map util/decrux)))
+
+(comment
+  (def node (xt/start-node {:xtdb.lucene/lucene-store {}}))
+
+  (->> (clojure.java.io/resource "sample_rss_news.edn")
+       (util/load-edn)
+       (xt/submit-tx node))
+
+  (xt/q (xt/db node)
+        {:find '[?e ?v ?s]
+         :where '[[(text-search :language "en") [[?e ?v ?s]]]
+                  [?e :xt/id]]})
+
+  (xt/q (xt/db node)
+        {:find '[?e ?v ?s]
+         :where '[[(text-search :description "de*") [[?e ?v ?s]]]
+                  [?e :xt/id]]})
+
+  (xt/q (xt/db node)
+        {:find '[?e ?v ?a ?s]
+         :where '[[(wildcard-text-search "ron*") [[?e ?v ?a ?s]]]
+                  [?e :xt/id]]}))
+
